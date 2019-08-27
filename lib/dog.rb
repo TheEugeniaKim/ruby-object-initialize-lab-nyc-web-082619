e9403dc5221d6671ebe9e45d9362537f8ac33022
Eugenia Kim <elk303@nyu.edu>
2019-08-27 14:51:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,9 @@ class Dog
   def initialize(name, breed)
     @name = name 
     @breed = breed
+    if breed=nil 
+    @breed="Mutt"
+  end
   end
   
   def name=(name)
@@ -20,8 +23,6 @@ class Dog
     @breed
   end
   
-  if breed=nil 
-    @breed="Mutt"
-  end
+  
   
 end
